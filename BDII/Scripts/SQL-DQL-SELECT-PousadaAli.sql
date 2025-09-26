b817885ee4a766a995b8f83ae5fb60ab11e0e3fa
Danilo Farias <dansoaresfarias@gmail.com>
2025-09-25 21:27:58 -0300

    Create SQL-DQL-SELECT-PousadaAli.sql

---
+++ BDII/Scripts/SQL-DQL-SELECT-PousadaAli.sql
@@ -0,0 +1,174 @@
+-- SQL - DQL
+select * from funcionario;
+
+select nome, cpf, carteiraTrab, chavePix, dataNasc, estadoCivil,
+	genero, cargaHoraria, salario
+	from funcionario;
+    
+select nome, cpf, carteiraTrab, chavePix, dataNasc, estadoCivil,
+	genero, cargaHoraria, salario
+	from funcionario
+		order by nome;
+
+select nome, cpf, carteiraTrab, chavePix, dataNasc, estadoCivil,
+	genero, cargaHoraria, salario
+	from funcionario
+		order by salario;
+
+select nome, cpf, carteiraTrab, chavePix, dataNasc, estadoCivil,
+	genero, cargaHoraria, salario
+	from funcionario
+		order by salario desc;
+
+select nome "Funcionário", cpf as "CPF", 
+	carteiraTrab "Carteira de Trabalho", 
+    chavePix "Chave PIX", 
+    dataNasc "Data de Nascimento", 
+    estadoCivil "Estado Civil", genero "Gênero", 
+    cargaHoraria "Carga Horária", salario "Salário"
+	from funcionario
+		order by nome;
+
+-- https://dev.mysql.com/doc/refman/8.4/en/string-functions.html#function_replace
+-- https://dev.mysql.com/doc/refman/8.4/en/date-and-time-functions.html#function_date-format
+-- https://dev.mysql.com/doc/refman/8.4/en/string-functions.html#function_upper
+-- https://dev.mysql.com/doc/refman/8.4/en/string-functions.html#function_ucase
+-- https://dev.mysql.com/doc/refman/8.4/en/string-functions.html#function_concat
+-- https://dev.mysql.com/doc/refman/8.4/en/string-functions.html#function_format
+select nome "Funcionário", 
+	replace(replace(cpf, '.', ''), '-', '') as "CPF", 
+	replace(carteiraTrab, '-', '') "Carteira de Trabalho", 
+    chavePix "Chave PIX", 
+    date_format(dataNasc, '%d/%m/%Y') "Data de Nascimento", 
+    upper(estadoCivil) "Estado Civil", ucase(genero) "Gênero", 
+    concat(cargaHoraria, 'h') "Carga Horária", 
+    concat("R$ ", format(salario, 2, 'de_DE')) "Salário"
+	from funcionario
+		order by nome;
+
+select nome "Funcionário", 
+	replace(replace(cpf, '.', ''), '-', '') as "CPF", 
+	replace(carteiraTrab, '-', '') "Carteira de Trabalho", 
+    chavePix "Chave PIX", 
+    date_format(dataNasc, '%d/%m/%Y') "Data de Nascimento", 
+    upper(estadoCivil) "Estado Civil", ucase(genero) "Gênero", 
+    concat(cargaHoraria, 'h') "Carga Horária", 
+    concat("R$ ", format(salario, 2, 'de_DE')) "Salário"
+	from funcionario
+		where genero = "Feminino" and 
+			salario between 3000 and 5000
+			order by nome;
+
+select nome "Funcionário", 
+	replace(replace(cpf, '.', ''), '-', '') as "CPF", 
+	replace(carteiraTrab, '-', '') "Carteira de Trabalho", 
+    chavePix "Chave PIX", 
+    date_format(dataNasc, '%d/%m/%Y') "Data de Nascimento", 
+    upper(estadoCivil) "Estado Civil", ucase(genero) "Gênero", 
+    concat(cargaHoraria, 'h') "Carga Horária", 
+    concat("R$ ", format(salario, 2, 'de_DE')) "Salário"
+	from funcionario
+		where estadoCivil like "Casad%" and
+			salario < 4000
+			order by nome;
+            
+select nome "Funcionário", 
+	replace(replace(cpf, '.', ''), '-', '') as "CPF", 
+	replace(carteiraTrab, '-', '') "Carteira de Trabalho", 
+    chavePix "Chave PIX", 
+    date_format(dataNasc, '%d/%m/%Y') "Data de Nascimento", 
+    upper(estadoCivil) "Estado Civil", ucase(genero) "Gênero", 
+    concat(cargaHoraria, 'h') "Carga Horária", 
+    concat("R$ ", format(salario, 2, 'de_DE')) "Salário"
+	from funcionario
+		where estadoCivil in ("Casado", "Casada")  and
+			salario < 4000
+			order by nome;
+
+select nome "Funcionário", 
+	replace(replace(cpf, '.', ''), '-', '') as "CPF", 
+	replace(carteiraTrab, '-', '') "Carteira de Trabalho", 
+    chavePix "Chave PIX", 
+    date_format(dataNasc, '%d/%m/%Y') "Data de Nascimento", 
+    upper(estadoCivil) "Estado Civil", ucase(genero) "Gênero", 
+    concat(cargaHoraria, 'h') "Carga Horária", 
+    concat("R$ ", format(salario, 2, 'de_DE')) "Salário"
+	from funcionario
+		where estadoCivil in ("Divorciada", "Casada")  and
+			salario > 4000
+			order by nome;
+
+select nome "Funcionário", 
+	replace(replace(cpf, '.', ''), '-', '') as "CPF", 
+	replace(carteiraTrab, '-', '') "Carteira de Trabalho", 
+    chavePix "Chave PIX", 
+    date_format(dataNasc, '%d/%m/%Y') "Data de Nascimento", 
+    upper(estadoCivil) "Estado Civil", ucase(genero) "Gênero", 
+    concat(cargaHoraria, 'h') "Carga Horária", 
+    concat("R$ ", format(salario, 2, 'de_DE')) "Salário"
+	from funcionario
+		where email like "%@gmail.com"
+			order by nome;
+            
+select nome "Dependente",
+	timestampdiff(year, dataNasc, now()) "Idade",
+    replace(replace(cpf, '.', ''), '-', '') as "CPF",
+    upper(parentesco) "Parentesco"
+	from dependente
+		order by idade, nome;
+    
+select d.nome "Dependente",
+	timestampdiff(year, d.dataNasc, now()) "Idade",
+    replace(replace(d.cpf, '.', ''), '-', '') as "CPF",
+    upper(d.parentesco) "Parentesco",
+    f.nome "Reponsável"
+	from dependente d, funcionario f
+		order by d.nome;
+
+select d.nome "Dependente",
+	timestampdiff(year, d.dataNasc, now()) "Idade",
+    replace(replace(d.cpf, '.', ''), '-', '') as "CPF",
+    upper(d.parentesco) "Parentesco",
+    f.nome "Reponsável"
+	from dependente d, funcionario f
+		where d.funcionario_cpf = f.cpf
+			order by d.nome;
+
+select d.nome "Dependente",
+	timestampdiff(year, d.dataNasc, now()) "Idade",
+    replace(replace(d.cpf, '.', ''), '-', '') as "CPF",
+    upper(d.parentesco) "Parentesco",
+    f.nome "Reponsável"
+	from dependente d
+		inner join funcionario f on d.funcionario_cpf = f.cpf
+			order by d.nome;
+
+select nome "Funcionário", 
+	replace(replace(cpf, '.', ''), '-', '') as "CPF", 
+	replace(carteiraTrab, '-', '') "Carteira de Trabalho", 
+    chavePix "Chave PIX", 
+    date_format(dataNasc, '%d/%m/%Y') "Data de Nascimento", 
+    upper(estadoCivil) "Estado Civil", ucase(genero) "Gênero",
+    cidade "Cidade",
+    concat(cargaHoraria, 'h') "Carga Horária", 
+    concat("R$ ", format(salario, 2, 'de_DE')) "Salário"
+	from funcionario
+		inner join endereco on funcionario_cpf = cpf
+		order by nome;
+        
+select nome "Funcionário", 
+	replace(replace(cpf, '.', ''), '-', '') as "CPF", 
+	replace(carteiraTrab, '-', '') "Carteira de Trabalho", 
+    chavePix "Chave PIX", 
+    date_format(dataNasc, '%d/%m/%Y') "Data de Nascimento", 
+    upper(estadoCivil) "Estado Civil", ucase(genero) "Gênero",
+    bairro "Bairro", cidade "Cidade",
+    concat(cargaHoraria, 'h') "Carga Horária", 
+    concat("R$ ", format(salario, 2, 'de_DE')) "Salário"
+	from funcionario
+		inner join endereco on funcionario_cpf = cpf
+			where cidade = "Recife" and bairro like "Boa%"
+				order by nome;
+
+
+
